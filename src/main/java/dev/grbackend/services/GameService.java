5232e8d42f57969d5a25dd29375ef8785c8b1ae2
Guille <gmarcg00@estudiantes.unileon.es>
2023-05-05 19:58:55 +0200

    GR-20 Cucumber test modified

--- src/main/java/dev/grbackend/services/GameService.java
+++ src/main/java/dev/grbackend/services/GameService.java
@@ -3,28 +3,16 @@ package dev.grbackend.services;
 import dev.grbackend.models.Game;
 import dev.grbackend.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 @Service
 public class GameService {
     @Autowired
     GameRepository gameRepository;
-    public ResponseEntity<Iterable<Game>> getGames(){
-        return new ResponseEntity<>(gameRepository.findAll(), HttpStatus.OK);
-        /*
-        String url = "https://api.rawg.io/api/games/the-last-of-us?key=66d0c7fd878844b6b062fe3f3822a772";
-        HttpHeaders headers = new HttpHeaders();
-
-        HttpEntity request = new HttpEntity(headers);
-        ResponseEntity<Game> response = restTemplate.exchange(url, HttpMethod.GET,request, Game.class);
-        Game game = response.getBody();
-        if(game!=null){
-            gameRepository.save(game);
-        }
-        return null;
-         */
+    public ArrayList<Game> getGames(){
+        return (ArrayList<Game>) gameRepository.findAll();
     }
 
     public Game saveGame(Game game){
--- src/test/java/dev/grbackend/RunCucumberTest.java
+++ src/test/java/dev/grbackend/RunCucumberTest.java
@@ -1,6 +1,9 @@
 package dev.grbackend;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.suite.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -10,5 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("dev/grbackend")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dev.grbackend")
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class RunCucumberTest {
 }
